2c998804e950ca0851fa44d1d348a417c4832a4d
Tim Urista <tim.urista@healthtrio.com>
2017-02-19 19:24:57 -0700

    master updated VRShopper.jsx??; 1 file changed, 13 insertions(+), 4 deletions(-)

--- src/pages/VRShopper.jsx
+++ src/pages/VRShopper.jsx
@@ -8,12 +8,17 @@ import {Entity, Scene} from 'aframe-react'
 export default class VRShopper extends Component {
     render() {
         return (
-      <div className="App">
-        <p>Options will go here</p>
+      
         
         <Scene>
+          <Entity
+            geometry={{primitive: 'sphere', radius: 100}}
+            material={{
+                shader: 'flat', 
+                src: 'url(https://rawgit.com/aframevr/assets/gh-pages/360-image-gallery-boilerplate/img/sechelt.jpg)'}}
+            scale="1 1 -1"/>
           <Entity 
-            geometry={{primitive: 'plane', height: 1, width: 1}}
+            geometry={{primitive: 'box', height: 1, width: 1}}
             material="color: blue"
             position={[0, 0, -5]} />
             
@@ -29,9 +34,13 @@ export default class VRShopper extends Component {
           position={[2, 2, -2]}
           onClick={() => { console.log('you clicked me'); this.object3D.rotation.x += .001} }
         />
+        
+        {/* SKY COMPONENT TODO: refactr */}
+        
+        {/* TODO: this for upload (https://eventphotosolutions.files.wordpress.com/2010/10/living-room-3601.jpg */}
+        {/* https://rawgit.com/aframevr/assets/gh-pages/360-image-gallery-boilerplate/img/sechelt.jpg */}
         {/* <Entity bmfont-text={{text: 'HELLO WORLD'}} position="{[0, 1, -5]}"/> */}
       </Scene>
-      </div>
     )
     }
 }
