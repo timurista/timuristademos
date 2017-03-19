7e52c556fdef79c0585807e644508d95cfabcfeb
Tim Urista <timothy.urista@gmail.com>
2017-03-18 19:52:38 -0700

    remove some unused variables

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
-import React, { Component } from 'react'
-// import logo from './logo.svg'
+import React, {Component} from 'react'
 import './App.css'
-// import { IndexLink, Link } from 'react-router'
 import AppHeader from './AppHeader'
-// import Breadcrumbs from 'react-breadcrumbs'
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
-
 class App extends Component {
     getChildContext() {
-        return { muiTheme: getMuiTheme(baseTheme) }
-    }    
+        return {muiTheme: getMuiTheme(baseTheme)}
+    }
 
     render() {
         return (
-          
-          
-      <div className="App">
-        <AppHeader />
-        <div className="App-header">
-          MAIN PAGE
-          {this.props.children}          
-        </div>
-      </div>
-    )
+            <div className="App">
+                <AppHeader/>
+                <div className="App-header">
+                    MAIN PAGE {this.props.children}
+                </div>
+            </div>
+        )
     }
 }
 
 App.childContextTypes = {
-    muiTheme: React.PropTypes.object.isRequired,
+    muiTheme: React.PropTypes.object.isRequired
 }
-
 
 export default App
