f0ec32721db756f00db41075b8ad3d01138ddfbc
Tim Urista <timothy.urista@gmail.com>
2017-03-19 09:43:13 -0700

    added basic login form

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
 class App extends Component {
-    getChildContext() {
-        return {muiTheme: getMuiTheme(baseTheme)}
-    }
+		getChildContext() {
+				return {muiTheme: getMuiTheme(baseTheme)}
+		}
 
-    render() {
-        return (
-            <div className="App">
-                <AppHeader/>
-                <div className="App-header">
-                    MAIN PAGE {this.props.children}
-                </div>
-            </div>
-        )
-    }
+		render() {
+				return (
+						<div className="App">
+								<AppHeader/>
+								<div className="App-header">
+										MAIN PAGE {this.props.children}
+								</div>
+						</div>
+				)
+		}
 }
 
 App.childContextTypes = {
-    muiTheme: React.PropTypes.object.isRequired
+		muiTheme: React.PropTypes.object.isRequired
 }
 
 export default App
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+
+class BuggyForm extends Component {
+    renderInputField(name, label) {
+        return (
+            <div>
+                <label>{label}</label>
+                <div>
+                    <Field name={name} component="input" type="text" placeholder={label}/>
+                </div>
+            </div>
+        )
+    }
+    submit(values) {
+        if (!values.city) {
+            // throw new Error('INTERNAL FAULT')
+            alert('Submit Failed: You must enter a valid city')
+            return null
+        }
+        return null;
+    }
+    render() {
+        const {handleSubmit, pristine, reset, submitting} = this.props
+        return (
+            <form onSubmit={handleSubmit(this.submit)}>
+                <h2>Search by</h2>
+                <p>
+                    Notice that if you enter a valid city, the submission will fail. Why is this?
+                </p>
+                {this.renderInputField('username', 'Username')}
+                {this.renderInputField('password', 'Password')}
+                AUTHENTICATION HOOKED UP
+                <div>
+                    <button type="submit" disabled={pristine || submitting}>Submit</button>
+                    <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+                </div>
+            </form>
+        )
+    }
+}
+
+export default reduxForm({
+    form: 'buggyForm' // a unique identifier for this form
+})(BuggyForm)
